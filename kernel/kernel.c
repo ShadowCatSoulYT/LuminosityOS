401e2545a10ccf76a1ea91af7734fa15efd8d62c
TheShadowCat <smile.wining.win@gmail.com>
2026-01-07 13:24:31 +0100

    Create kernel.c

---
+++ kernel/kernel.c
@@ -0,0 +1,14 @@
+void print(const char* str) {
+    unsigned short* video_memory = (unsigned short*)0xB8000;
+
+    for (int i = 0; str[i] != '\0'; ++i) {
+        video_memory[i] = (video_memory[i] & 0xFF00) | str[i];
+    }
+}
+
+void kernel_main() {
+    print("Welcome to LuminosityOS!");
+    
+    while(1) {
+    }
+}  
