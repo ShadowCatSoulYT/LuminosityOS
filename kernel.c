42f0bd159dfb0dd1bcb144c3bc9f8ecdea574278
TheShadowCat <smile.wining.win@gmail.com>
2026-01-07 11:49:05 +0100

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,17 +1,22 @@
 void print(const char* str) {
-    // Pointer to the start of VGA text memory
     unsigned short* video_memory = (unsigned short*)0xB8000;
 
     for (int i = 0; str[i] != '\0'; ++i) {
-        // We keep the existing color/attribute (the top 8 bits)
-        // and only change the character (the bottom 8 bits)
         video_memory[i] = (video_memory[i] & 0xFF00) | str[i];
     }
 }
 
 void kernel_main() {
-    print("Hello from the Kernel!");
+    print("Welcome to LuminosityOS!");
+    print("-------------------------");
+    print("We are going to install system files...");
+    //code will go here
+    print("Installation has finished, awaiting boot into graphics mode");
+    print("While you wait, here is my 'thanks you's!");
+    print("Thanks to KolibriOS and its devs for inspiration");
+    print("Thanks to my friends for being supportive");
+    print("And thank you for making this possible ;)");
+    
     while(1) {
-        // Halt or loop forever
     }
 }  
