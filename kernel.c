bb04584990493f72723c2ad442a71eb51062bc7f
TheShadowCat <smile.wining.win@gmail.com>
2026-01-07 11:41:34 +0100

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,25 +1,17 @@
-void put_pixel(int x, int y, unsigned char color) {
-    unsigned char* vga = (unsigned char*)0xA0000;
-    vga[y * 320 + x] = color;
+void print(const char* str) {
+    // Pointer to the start of VGA text memory
+    unsigned short* video_memory = (unsigned short*)0xB8000;
+
+    for (int i = 0; str[i] != '\0'; ++i) {
+        // We keep the existing color/attribute (the top 8 bits)
+        // and only change the character (the bottom 8 bits)
+        video_memory[i] = (video_memory[i] & 0xFF00) | str[i];
+    }
 }
 
 void kernel_main() {
-    // Fill screen with a clear red color (color 4 in standard VGA palette)
-    // to verify graphics are working and the kernel is actually running.
-    for (int y = 0; y < 200; y++) {
-        for (int x = 0; x < 320; x++) {
-            put_pixel(x, y, 4);
-        }
-    }
-    
-    // Draw a small white square in the center
-    for (int y = 90; y < 110; y++) {
-        for (int x = 150; x < 170; x++) {
-            put_pixel(x, y, 15);
-        }
-    }
-    
+    print("Hello from the Kernel!");
     while(1) {
-        __asm__("hlt");
+        // Halt or loop forever
     }
-}
+}  
