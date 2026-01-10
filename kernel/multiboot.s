9efd884a0bf7a1fe6510ca40f31384ffb63aefe0
TheShadowCat <smile.wining.win@gmail.com>
2026-01-10 08:38:03 +0100

    Create multiboot.s

---
+++ kernel/multiboot.s
@@ -0,0 +1,28 @@
+GImake ; multiboot.s
+MULTIBOOT_MAGIC equ 0x1BADB002
+MULTIBOOT_FLAGS equ 0x00000000
+MULTIBOOT_CHECKSUM equ -(MULTIBOOT_MAGIC + MULTIBOOT_FLAGS)
+
+section .multiboot
+align 4
+    dd MULTIBOOT_MAGIC
+    dd MULTIBOOT_FLAGS
+    dd MULTIBOOT_CHECKSUM
+
+section .text
+global _start
+extern kernel_main
+
+_start:
+    cli
+    mov esp, stack_top
+    call kernel_main
+    hlt
+.hang:
+    jmp .hang
+
+section .bss
+align 16
+stack_bottom:
+    resb 16384
+stack_top:
